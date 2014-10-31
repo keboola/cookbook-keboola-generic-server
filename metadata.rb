25f73093dd6a6907c329c9d20d3fd881ee7249ad
Martin Halamíček <martin@halamicek.cz>
2014-10-31 15:19:51 +0100

    PHP55

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures keboola-generic-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 
 depends 'aws', '~> 2.4.0'
-depends 'keboola-php'
+depends 'keboola-php55'
 depends 'keboola-common'
 depends 'keboola-apache2'
