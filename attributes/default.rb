87a399ee3fb5302d1085e6c26c529c8f6ebd1b36
Martin Halamíček <martin@halamicek.cz>
2014-10-31 18:29:03 +0100

    php5.5 modules

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 
-default['php']['packages'] = %w{ php55 php55-devel php-pear php55-pdo php55-mysql php55-mbstring php55-mcrypt php54-pecl-apcu }
+default['php']['packages'] = %w{ php55 php55-opcache php55-devel php-pear php55-pdo php55-mysqlnd php55-mbstring php55-mcrypt php55-pecl-apcu }
